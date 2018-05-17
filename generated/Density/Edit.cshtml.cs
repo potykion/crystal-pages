6f78e8ba3de642465ae057c1e3f2af1a2669778c
potykion <potykion@gmail.com>
2018-05-17 17:54:39 +0300

    - Hide Edit/Delete links for unauthorized users - Authorization import

--- generated/Density/Edit.cshtml.cs
+++ generated/Density/Edit.cshtml.cs
@@ -1,6 +1,7 @@
 using System.Linq;
 using System.Threading.Tasks;
 using Crystal.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
--- generated/Solubility/Delete.cshtml.cs
+++ generated/Solubility/Delete.cshtml.cs
@@ -1,6 +1,7 @@
 using System.Threading.Tasks;
 using Crystal.Models;
 using Crystal.Utils;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
